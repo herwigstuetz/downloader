5c0ccf6bff592120265fd7ab7fdcc481487b61d9
Herwig Stuetz <herwig@herwigstuetz.com>
2020-12-18 20:13:45 +0000

    Add main

--- src/lib.rs
+++ src/lib.rs
@@ -14,26 +14,41 @@ pub enum Error {
 type Result<T> = std::result::Result<T, Error>;
 
 #[no_mangle]
-fn download(url: String, tmp: String) -> Result<String> {
-    let mut response = match reqwest::blocking::get(&url) {
+pub fn download(url: &str, tmp: &str) -> Result<String> {
+
+    // Make GET request for url
+    let mut response = match reqwest::blocking::get(url) {
         Ok(response) => response,
         Err(err) => return Err(Error::Request(err)),
     };
 
-    let fname = &response
-        .url()
-        .path_segments()
-        .and_then(|segments| segments.last())
-        .and_then(|name| if name.is_empty() { None } else { Some(name) })
-        .unwrap_or("tmp.bin");
+    // Get Content-Disposition header:
+    // > Content-Disposition: attachment; filename="filename.jpg"
+    let content_disposition = response
+        .headers()
+        .get("Content-Disposition")
+        .and_then(|header| header.to_str().ok())
+        .unwrap_or("");
 
-    let fname = Path::new(&tmp).join(fname);
+    // Get filename from Content-Disposition
+    let filename : String = content_disposition
+        .split("; ")
+        .filter(|s| s.contains("filename"))
+        .collect::<String>()
+        .split("=")
+        .collect::<Vec<&str>>()[1].to_string();
+    println!("headers: {:#?}", &filename);
 
+    // Append filename to tmp directory
+    let fname = Path::new(tmp).join(filename);
+
+    // Open destination file
     let mut dest = match File::create(&fname) {
         Ok(file) => file,
         Err(err) => return Err(Error::File(err)),
     };
 
+    // Write data from response (attachment) into destination
     match io::copy(&mut response, &mut dest) {
         Ok(_) => Ok(fname.into_os_string().into_string().unwrap()),
         Err(err) => Err(Error::File(err)),
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::vec::Vec;
+use std::string::String;
+
+use downloader::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+
+    let url = &args[1];
+
+    println!("Attempting to download url {}", url);
+    match download(url, ".") {
+        Ok(file) => println!("Downloaded file to {}", file),
+        Err(_err) => println!("Could not download file"),
+    };
 }
